8d80a9fdd5d6721695b064d404ede03f8e35d889
Rostyslav Stepanchuk <ros_st@ukr.net>
2019-05-11 14:42:43 +0300

    createNote form draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header'
 import Cards from './components/Cards'
+import FormNote from './components/FormNote'
 import Footer from './components/Footer'
 
 function App() {
--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import {WiredButton} from 'wired-elements';
 
 Button.propTypes = {
-    title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    onClick: PropTypes.func
 };
 
 function Button(props) {
     return (
-        <wired-button>
+        <wired-button onClick ={props.onClick}>
             {props.title}
         </wired-button>
     );
