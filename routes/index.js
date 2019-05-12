aae5153dda75fc55ab8b218bfd8dc18bc64c8c01
Rostyslav Stepanchuk <ros_st@ukr.net>
2019-05-13 00:52:30 +0300

    Mongo operations addOne,getCollection,updateOne,deleteOne

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const db = require('../db');
 
 /* GET home page. */
-router.get('/test', function(req, res, next) {
-  console.log('works');
-  db.testConnection();
-  res.send('WORKS');
+router.get('/', function(req, res, next) {
+  // db.testConnection();
+  // db.addOne('users',{testName:'Slava'}).then(result=>res.send(result));
+  // db.updateOne('users',{id:"5cd88a2ba204f43f369cea65", update:{testName:'Olezhka',friend:'true'},delete:{sex:"",male:""}}).then(result=>res.send(result));
+db.deleteOne('users','5cd87d18d194c53ea2caaf06').then(result=>res.send(result));
+  // db.getFullCollection('users').then(data=>res.send(data));
 });
 
 module.exports = router;
