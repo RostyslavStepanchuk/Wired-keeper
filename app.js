fb9cdceb0c949df74be66532d57bb76d5ccc9aad
Rostyslav Stepanchuk <ros_st@ukr.net>
2019-05-28 14:59:23 +0300

    removing cors completely

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 
 
 const indexRouter = require('./routes/index');
@@ -15,15 +15,16 @@ app.use(logger('dev'));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
-app.options('*',cors());
+// app.use(cors());
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "client", "build")));
+// app.options('*',cors());
 
 // app.use((req,res,next)=>{
 //     res.setHeader("Access-Control-Allow-Origin",'http://localhost:3000');
 //     next();
 // });
-app.use(express.static(path.join(__dirname, "client", "build")));
+
 
 app.use('/', indexRouter);
 app.use('/notes', notesRouter);
